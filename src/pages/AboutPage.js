a9e5559f9ce897a8b76a2c0bf56c726c6ef023be
RudenkoArt <rudnkoart@gmail.com>
2023-02-15 11:18:56 +0200

    About page ready

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,11 +1,35 @@
-import React from 'react'
-import styled from 'styled-components'
-import { PageHero } from '../components'
-import aboutImg from '../assets/hero-bcg.jpeg'
+import React from 'react';
+import styled from 'styled-components';
+import { PageHero } from '../components';
+import aboutImg from '../assets/hero-bcg.jpeg';
 
 const AboutPage = () => {
-  return <h4>about page</h4>
-}
+  return (
+    <main>
+      <PageHero title='about' />
+      <Wrapper className='page section section-center'>
+        <img src={aboutImg} alt='nice desk' />
+        <article>
+          <div className='title'>
+            <h2>our story</h2>
+            <div className='underline'></div>
+            <p>
+              I'm baby woke tilde vaporware helvetica fashion axe unicorn.
+              Copper mug prism readymade quinoa pabst sustainable. Paleo vice
+              master cleanse hoodie vape, locavore sus trust fund mustache marfa
+              flannel pop-up venmo succulents. Vexillologist subway tile meh,
+              food truck post-ironic XOXO same lomo PBR&B JOMO deep v austin
+              hashtag. Intelligentsia kitsch hoodie, occupy semiotics tonx swag
+              Brooklyn +1 typewriter man braid sus pour-over meh. Meditation man
+              bun thundercats fam tonx, listicle photo booth knausgaard
+              helvetica locavore readymade swag mlkshk lyft pickled.
+            </p>
+          </div>
+        </article>
+      </Wrapper>
+    </main>
+  );
+};
 
 const Wrapper = styled.section`
   display: grid;
@@ -33,5 +57,5 @@ const Wrapper = styled.section`
   @media (min-width: 992px) {
     grid-template-columns: 1fr 1fr;
   }
-`
-export default AboutPage
+`;
+export default AboutPage;
