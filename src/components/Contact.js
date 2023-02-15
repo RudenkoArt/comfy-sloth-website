bba8605888383266a304dec6f232a33315f98cdf
RudenkoArt <rudnkoart@gmail.com>
2023-02-15 11:49:12 +0200

    Contact component ready

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,34 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 
 const Contact = () => {
-  return <h4>contact section</h4>
-}
+  return (
+    <Wrapper>
+      <div className='section-center'>
+        <h3>Join our newsletter and get 20% off</h3>
+        <div className='content'>
+          <p>
+            Tattooed godard hexagon, meh poke coloring book fixie man bun
+            farm-to-table tumblr health goth iPhone letterpress cornhole
+            crucifix. Raclette everyday carry austin seitan pop-up tumblr four
+            dollar toast asymmetrical echo park 3 wolf moon stumptown craft
+            beer.
+          </p>
+          <form className='contact-form'>
+            <input
+              type='email'
+              className='form-input'
+              placeholder='enter email'
+            />
+            <button type='submit' className='submit-btn'>
+              subscribe
+            </button>
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
 const Wrapper = styled.section`
   padding: 5rem 0;
   h3 {
@@ -23,6 +48,7 @@ const Wrapper = styled.section`
 
   .form-input,
   .submit-btn {
+    outline: none;
     font-size: 1rem;
     padding: 0.5rem 1rem;
     border: 2px solid var(--clr-black);
@@ -67,6 +93,6 @@ const Wrapper = styled.section`
   @media (min-width: 1280px) {
     padding: 15rem 0;
   }
-`
+`;
 
-export default Contact
+export default Contact;
