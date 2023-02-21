0c2de087134943be2fd5e4ac1d269e15446dcc59
RudenkoArt <rudnkoart@gmail.com>
2023-02-21 15:27:47 +0200

    implement Auth0 authentication

--- src/index.js
+++ src/index.js
@@ -12,11 +12,20 @@ import { Auth0Provider } from '@auth0/auth0-react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>
+  <Auth0Provider
+    domain='dev-kemsp7apehdhfnaf.us.auth0.com'
+    clientId='PuaPUFlpGtrb32ZiZO9klhIfVta4E6we'
+    redirectUri={window.location.origin}
+    cacheLocation='localstorage'
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>
 );
